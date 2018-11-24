0d3816408a6c5ddb2d03683c01dd7f02a6f9d488
Jean <moimog33@gmail.com>
2018-11-24 16:49:46 +1300

    Fix ci tests

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,20 +3,26 @@ set -ev
 
 # Test ContentaCMS
 ddev exec drush status
+
 curl -H "Access-Control-Request-Method: GET" -H "Origin: http://contenta.ddev.local" -I http://contenta.ddev.local/api
-# curl -H "Access-Control-Request-Method: GET" -H "Origin: http://contenta.ddev.local" http://contenta.ddev.local/api
-# curl -H "Access-Control-Request-Method: GET" -H "Origin: http://contenta.ddev.local" http://contenta.ddev.local/api/pages
+
+curl -H "Access-Control-Request-Method: GET" -H "Origin: http://contenta.ddev.local" http://contenta.ddev.local/api | jq '.links.self'
+
+curl -H "Access-Control-Request-Method: GET" -H "Origin: http://contenta.ddev.local" http://contenta.ddev.local/api/pages | jq '.jsonapi.version'
 
 # Test ContentaJS
 ddev logs -s pm2
+
 curl -H "Access-Control-Request-Method: GET" -H "Origin: http://contenta.ddev.local" -I http://contenta.ddev.local:3000/api
-# curl -H "Access-Control-Request-Method: GET" -H "Origin: http://contenta.ddev.local" http://contenta.ddev.local:3000/api
-# curl -H "Access-Control-Request-Method: GET" -H "Origin: http://contenta.ddev.local" http://contenta.ddev.local:3000/api/pages
+
+curl -H "Access-Control-Request-Method: GET" -H "Origin: http://contenta.ddev.local" http://contenta.ddev.local:3000/api | jq '.links.self'
+
+curl -H "Access-Control-Request-Method: GET" -H "Origin: http://contenta.ddev.local" http://contenta.ddev.local:3000/api/pages | jq '.jsonapi.version'
 
 # Test Front VUE
-ddev logs -s vue_nuxt
+ddev logs -s front_vue
+
 curl -I http://front-vue.ddev.local
-curl -I http://front-vue.ddev.local 2>/dev/null | head -n 1 | cut -d$' ' -f2
 
 # Test Redis
-curl -I http://contenta.ddev.local:8081 2>/dev/null | head -n 1 | cut -d$' ' -f2
+curl -I http://contenta.ddev.local:8081
